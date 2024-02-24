5eaa8a916b8724bd9e6782f3a54f209e63cf515a
Bhuwadit Athikraikun <bhuwaditathikraikun@Bhuwadits-MacBook-Pro.local>
2024-02-25 03:51:18 +0700

    register update

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,6 +1,6 @@
 import { Button, Container, Row, Col, Form } from "react-bootstrap";
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 
 import axios from "axios";
 import "./LoginPage.css";
@@ -37,21 +37,19 @@ const LoginPage = ({ loginFunction }) => {
 
   return (
     <Container className="d-flex vh-100">
-      <Row className="m-auto aligh-self-center w-50">
+      <Row className="m-auto aligh-self-center w-50 h-50">
         <Form className="border bg-light rounded-3 p-5" onSubmit={handleSubmit}>
-          <Form.Label className="text-center w-100 h2">Login</Form.Label>
+          <Form.Label className="text-center w-100 h2 p-3">Sign In</Form.Label>
           <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>Username</Form.Label>
             <Form.Control
               type="email"
-              placeholder="Enter Username"
+              placeholder="Email"
               value={ID}
               onChange={handleIDChange}
             />
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
             <Form.Control
               type="password"
               placeholder="Password"
@@ -59,17 +57,22 @@ const LoginPage = ({ loginFunction }) => {
               onChange={handlePasswordChange}
             />
           </Form.Group>
+          {/* <Form.Group>
+            <Form.Check // prettier-ignore
+              type={"checkbox"}
+              label="Remember me?"
+            />
+          </Form.Group> */}
           <Row className="m-auto w-75">
-            <Col>
-              <Button className="login-btn w-100" type="submit">
-                Login
-              </Button>
-            </Col>
-            <Col>
-              <Button className="login-btn w-100 " onClick={navigateRegister}>
-                Register
-              </Button>
-            </Col>
+            <Button className="login-btn w-100" type="submit">
+              Login
+            </Button>
+          </Row>
+          <Row className="m-auto w-50"></Row>
+          <Row className=" w-100 text-end">
+            <Form>
+              Need an account? <Link to="/register">Sign Up</Link>
+            </Form>
           </Row>
         </Form>
       </Row>
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,5 +1,81 @@
+import { Button, Container, Row, Col, Form } from "react-bootstrap";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 const Register = () => {
-  return <div>Register</div>;
+  const [ID, setID] = useState();
+  const [password, setPassword] = useState();
+  const [name, setName] = useState();
+  const [surname, setSurname] = useState();
+  const handleIDChange = (event) => {
+    setID(event.target.value);
+  };
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
+  const handleSurnameChange = (event) => {
+    setSurname(event.target.value);
+  };
+  return (
+    <Container className="d-flex vh-100">
+      <Row className="m-auto aligh-self-center w-50">
+        <Form className="border bg-light rounded-3 p-5">
+          <Form.Label className="text-center w-100 h2">Sign Up</Form.Label>
+          <Form.Group className="mb-3 " controlId="formBasicEmail">
+            <Form.Control
+              type="email"
+              placeholder="Email(This will be your username)"
+              value={ID}
+              onChange={handleIDChange}
+            />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={handlePasswordChange}
+            />
+          </Form.Group>
+          <Row>
+            <Col>
+              <Form.Group className="mb-3">
+                <Form.Control
+                  type="text"
+                  placeholder="Your First Name"
+                  value={name}
+                  onChange={handleNameChange}
+                />
+              </Form.Group>
+            </Col>
+            <Col>
+              <Form.Group className="mb-3">
+                <Form.Control
+                  type="text"
+                  placeholder="Your Last Name"
+                  value={surname}
+                  onChange={handleSurnameChange}
+                />
+              </Form.Group>
+            </Col>
+          </Row>
+          <Row className="m-auto w-75">
+            <Col>
+              <Button className="login-btn w-100" type="submit">
+                Sign Up
+              </Button>
+            </Col>
+          </Row>
+          <Row>
+            <Col></Col>
+          </Row>
+        </Form>
+      </Row>
+    </Container>
+  );
 };
 
 export default Register;
